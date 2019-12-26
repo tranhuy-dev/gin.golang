0e7166edb01f6e029fa21bdc4492ddd989fc670d
Huy <tqhuy1996.developer@gmail.com>
2019-12-26 20:03:46 +0700

    first api with gin

--- main.go
+++ main.go
@@ -2,8 +2,25 @@
 package main
 
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/gin-gonic/gin"
+	// "net/http"
+	"gin.api/model"
+)
+
 
 func main() {
 	fmt.Println("hello world")
+	r := gin.Default()
+	r.GET("api/v1" , getData)
+
+	r.Run()
+}
+
+func getData(c *gin.Context) {
+	c.JSON(200, model.Response{
+		Message: "Api is working",
+		StatusCode: 200,
+	})
 }
---
+++ model/response.go
@@ -0,0 +1,6 @@
+package model
+
+type Response struct {
+	Message string `json:"message"`
+	StatusCode int `json:"status_code"`
+}
