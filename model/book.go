4731b7bd266b67ffbe94a614e775797362e9cb48
Huy <tqhuy1996.developer@gmail.com>
2019-12-27 10:22:34 +0700

    bson

--- model/book.go
+++ model/book.go
@@ -1,16 +1,16 @@
 package model
 
 type Book struct {
-	Name string `json:"name"`
-	Type string `json:"type"`
-	BookID int `json:"book_id"`
-	Status int `json:"status"`
-	Owner int `json:"owner"`
-	BorrowDate int `json:"borrow_date"`
-	PayDate int `json:"pay_date"`
-	Quality int `json:"quality"`
-	TotalBorrow int `json:"total_borrow"`
-	Description string `json:"description"`
-	Author string `json:"author"`
-	Image string `json:"image"`
+	Name string `json:"name" bson:"name"`
+	Type string `json:"type" bson:"type"`
+	BookID int `json:"book_id" bson:"book_id"`
+	Status int `json:"status" bson:"status"`
+	Owner int `json:"owner" bson:"owner"`
+	BorrowDate int `json:"borrow_date" bson:"borrow_date"`
+	PayDate int `json:"pay_date" bson:"pay_date"`
+	Quality int `json:"quality" bson:"quality"`
+	TotalBorrow int `json:"total_borrow" bson:"total_borrow"`
+	Description string `json:"description" bson:"description"`
+	Author string `json:"author" bson:"author"`
+	Image string `json:"image" bson:"image"`
 }
--- model/customer.go
+++ model/customer.go
@@ -1,17 +1,17 @@
 package model
 
 type Customer struct {
-	FirstName string `json:"first_name"`
-	LastName string `json:"last_name"`
-	Address string `json:"address"`
-	Status int `json:"status"`
-	Reputation int `json:"reputation"`
-	Image string `json:"image"`
-	Books []int `json:"books"`
-	Phone string `json:"phone"`
-	Friends []int `json:"friends"`
-	Password string `json:"password"`
-	Token string `json:"token"`
-	CustomerID int `json:"customer_id"`
+	FirstName string `json:"first_name" bson:"first_name"`
+	LastName string `json:"last_name" bson:"last_name"`
+	Address string `json:"address" bson:"address"`
+	Status int `json:"status" bson:"status"`
+	Reputation int `json:"reputation" bson:"reputation"`
+	Image string `json:"image" bson:"image"`
+	Books []int `json:"books" bson:"books"`
+	Phone string `json:"phone" bson:"phone"`
+	Friends []int `json:"friends" bson:"friends"`
+	Password string `json:"password" bson:"password"`
+	Token string `json:"token" bson:"token"`
+	CustomerID int `json:"customer_id" bson:"customer_id"`
 }
 
