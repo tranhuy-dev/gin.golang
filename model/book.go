9ef8b5754f60d7c4d7790ab362a9b815ea763aaf
Huy <tqhuy1996.developer@gmail.com>
2019-12-27 10:15:03 +0700

    book and customer model

--- model/book.go
+++ model/book.go
@@ -2,4 +2,15 @@ package model
 
 type Book struct {
 	Name string `json:"name"`
+	Type string `json:"type"`
+	BookID int `json:"book_id"`
+	Status int `json:"status"`
+	Owner int `json:"owner"`
+	BorrowDate int `json:"borrow_date"`
+	PayDate int `json:"pay_date"`
+	Quality int `json:"quality"`
+	TotalBorrow int `json:"total_borrow"`
+	Description string `json:"description"`
+	Author string `json:"author"`
+	Image string `json:"image"`
 }
--- model/customer.go
+++ model/customer.go
@@ -1,6 +1,17 @@
 package model
 
 type Customer struct {
-	Name string `json:"name"`
+	FirstName string `json:"first_name"`
+	LastName string `json:"last_name"`
+	Address string `json:"address"`
+	Status int `json:"status"`
+	Reputation int `json:"reputation"`
+	Image string `json:"image"`
+	Books []int `json:"books"`
+	Phone string `json:"phone"`
+	Friends []int `json:"friends"`
+	Password string `json:"password"`
+	Token string `json:"token"`
+	CustomerID int `json:"customer_id"`
 }
 
